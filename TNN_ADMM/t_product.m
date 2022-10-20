a8c0dcd6af72092c1d741e0879160eb5737bd13b
Zheng Tan <61394893+ZT220501@users.noreply.github.com>
2022-10-20 11:25:52 -0700

    Correct t-product

--- TNN_ADMM/t_product.m
+++ TNN_ADMM/t_product.m
@@ -1,30 +1,14 @@
 %Tensor-Tensor product
-%Return the t-product of tensor Z and X
+%Return the t-product of tensor A and B
 function C = t_product(A, B)
-    [n1,n2,n3] = size(A);
-    [m1,m2,m3] = size(B);
-    
-    if n2 ~= m1 || n3 ~= m3 
-        error('Inner tensor dimensions must agree.');
+    A_trans = fft(A, [], 3);
+    B_trans = fft(B, [], 3);
+
+    [N1, ~, N3] = size(A);
+    [~, N2, ~] = size(B);
+    C_trans = zeros(N1, N2, N3);
+    for i=1:N3
+        C_trans(:,:,i) = A_trans(:,:,i)*B_trans(:,:,i);
     end
-    
-    A = fft(A,[],3);
-    B = fft(B,[],3);
-    C = zeros(n1,m2,n3);
-    
-    % first frontal slice
-    C(:,:,1) = A(:,:,1)*B(:,:,1);
-    % i=2,...,halfn3
-    halfn3 = round(n3/2);
-    for i = 2 : halfn3
-        C(:,:,i) = A(:,:,i)*B(:,:,i);
-        C(:,:,n3+2-i) = conj(C(:,:,i));
-    end
-    
-    % if n3 is even
-    if mod(n3,2) == 0
-        i = halfn3+1;
-        C(:,:,i) = A(:,:,i)*B(:,:,i);
-    end
-    C = ifft(C,[],3);
+    C = ifft(C_trans, [], 3);
 end
